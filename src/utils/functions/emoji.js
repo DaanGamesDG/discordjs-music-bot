c129939d756f6dcbb6670325c0d802ee96539bfc
DaangamesDG <dwklarenbeek@gmail.com>
2020-11-01 19:13:13 +0100

    fixed unkown guild issue

--- src/utils/functions/emoji.js
+++ src/utils/functions/emoji.js
@@ -4,7 +4,7 @@
  * @param {string} emojiname name of the emoji
  */
 function emojiFinder(client, emojiname) {
-  let guild = client.guilds.cache.get('746536046275198997');
+  let guild = client.guilds.cache.get(process.env.GUILD_ID);
   let emoji = guild.emojis.cache.find(m => m.name.toLowerCase() == emojiname.toLowerCase());
   if (!emoji) throw new TypeError('Invalid emoji name!');
   return emoji;
